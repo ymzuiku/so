4ce7f4adcacabed91cc86e2fed395b37f683fa37
pillar <ymblender@gamil.com>
2021-07-18 12:33:07 +0800

    fix: add method document

--- so.go
+++ so.go
@@ -15,8 +15,9 @@ func line() string {
 	return fmt.Sprintf("\n%s:%d", file, line)
 }
 
-// this func copy from assert
-func isEmpty(object interface{}) bool {
+// Check object is empty
+// This func copy from assert
+func IsEmpty(object interface{}) bool {
 
 	// get nil case out of the way
 	if object == nil {
@@ -35,7 +36,7 @@ func isEmpty(object interface{}) bool {
 			return true
 		}
 		deref := objValue.Elem().Interface()
-		return isEmpty(deref)
+		return IsEmpty(deref)
 		// for all other types, compare against the zero value
 	default:
 		zero := reflect.Zero(objValue.Type())
@@ -43,54 +44,64 @@ func isEmpty(object interface{}) bool {
 	}
 }
 
+// Check value is true
 func True(t *testing.T, check bool) {
 	if !check {
 		t.Errorf("%v : Not True", line())
 	}
 }
 
+// Check value is false
 func False(t *testing.T, check bool) {
 	if check {
 		t.Errorf("%v : Not False", line())
 	}
 }
 
+// Check two values is equal
 func Equal(t *testing.T, a, b interface{}) {
 	if a != b && !reflect.DeepEqual(a, b) {
 		t.Errorf("%v : Not Equal: %v == %v", line(), a, b)
 	}
 }
 
+// Check two values isn't equal
 func NotEqual(t *testing.T, a, b interface{}) {
 	if a == b || reflect.DeepEqual(a, b) {
 		t.Errorf("%v : Equal: %v != %v", line(), a, b)
 	}
 }
 
+// Check object is empty
+// If len([]string) == 0, it's empty
 func Empty(t *testing.T, target interface{}) {
-	if !isEmpty(target) {
+	if !IsEmpty(target) {
 		t.Errorf("%v : Not Empty: %v", line(), target)
 	}
 }
 
+// Check object isn't empty
 func NotEmpty(t *testing.T, target interface{}) {
-	if isEmpty(target) {
+	if IsEmpty(target) {
 		t.Errorf("%v : Empty: %v", line(), target)
 	}
 }
 
+// Check object == nil
 func Nil(t *testing.T, stack interface{}) {
 	if stack != nil {
 		t.Errorf("%v : Not Nil", line())
 	}
 }
 
+// Check object != nil
 func NotNil(t *testing.T, stack interface{}) {
 	if stack == nil {
 		t.Errorf("%v : Nil: %v", line(), stack)
 	}
 }
 
+// Check error != nil
 func Error(t *testing.T, err error) {
 	if err == nil {
 		t.Errorf("%v : Not Error", line())

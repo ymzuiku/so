cef9ff02ea9b3b67443fde7b373c262afda16db0
pillar <ymblender@gamil.com>
2021-07-20 02:38:06 +0800

    fix: use new format

--- so.go
+++ so.go
@@ -2,31 +2,15 @@ package so
 
 import (
 	"fmt"
-	"os"
 	"reflect"
-	"regexp"
 	"runtime"
-	"sync"
 	"testing"
 )
-
-var createPwdRegOnce sync.Once
-
-var pwdReg *regexp.Regexp
-
-func createPwdReg() {
-	pwd := os.Getenv("pwd")
-	pwdReg = regexp.MustCompile(pwd)
-}
 
 func line() string {
 	_, file, line, ok := runtime.Caller(2)
 	if !ok {
 		return "[so]LineError runtime.Caller(2) Fail"
-	}
-	createPwdRegOnce.Do(createPwdReg)
-	if pwdReg != nil {
-		file = pwdReg.ReplaceAllString(file, "")
 	}
 
 	return fmt.Sprintf("\n%s:%d", file, line)
@@ -64,28 +48,28 @@ func IsEmpty(object interface{}) bool {
 // Check value is true
 func True(t *testing.T, check bool) {
 	if !check {
-		t.Errorf("%v : Not True", line())
+		t.Errorf("%v Not True", line())
 	}
 }
 
 // Check value is false
 func False(t *testing.T, check bool) {
 	if check {
-		t.Errorf("%v : Not False", line())
+		t.Errorf("%v Not False", line())
 	}
 }
 
 // Check two values is equal
 func Equal(t *testing.T, a, b interface{}) {
 	if a != b && !reflect.DeepEqual(a, b) {
-		t.Errorf("%v : Not Equal: %v == %v", line(), a, b)
+		t.Errorf("%v Not Equal: %v == %v", line(), a, b)
 	}
 }
 
 // Check two values isn't equal
 func NotEqual(t *testing.T, a, b interface{}) {
 	if a == b || reflect.DeepEqual(a, b) {
-		t.Errorf("%v : Equal: %v != %v", line(), a, b)
+		t.Errorf("%v Equal: %v != %v", line(), a, b)
 	}
 }
 
@@ -93,34 +77,34 @@ func NotEqual(t *testing.T, a, b interface{}) {
 // If len([]string) == 0, it's empty
 func Empty(t *testing.T, target interface{}) {
 	if !IsEmpty(target) {
-		t.Errorf("%v : Not Empty: %v", line(), target)
+		t.Errorf("%v Not Empty: %v", line(), target)
 	}
 }
 
 // Check object isn't empty
 func NotEmpty(t *testing.T, target interface{}) {
 	if IsEmpty(target) {
-		t.Errorf("%v : Empty: %v", line(), target)
+		t.Errorf("%v Empty: %v", line(), target)
 	}
 }
 
 // Check object == nil
 func Nil(t *testing.T, stack interface{}) {
 	if stack != nil {
-		t.Errorf("%v : Not Nil: %v", line(), stack)
+		t.Errorf("%v Not Nil: %v", line(), stack)
 	}
 }
 
 // Check object != nil
 func NotNil(t *testing.T, stack interface{}) {
 	if stack == nil {
-		t.Errorf("%v : Nil", line())
+		t.Errorf("%v Nil", line())
 	}
 }
 
 // Check error != nil
 func Error(t *testing.T, err error) {
 	if err == nil {
-		t.Errorf("%v : Not Error", line())
+		t.Errorf("%v Not Error", line())
 	}
 }
